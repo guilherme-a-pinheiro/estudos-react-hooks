0c317fb6cb1476f61834e20eb1da4f841c171a77
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-02-27 20:37:19 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
-  const [reverse, setReverse] = useState(false);
+  // componentDidUpdate - executa toda vez que o component atualiza
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
 
-  const reverseClass = reverse ? 'reverse' : '';
+  // // componentDidMount - executa 1x
+  // useEffect(() => {
+  //   console.log('componentDidMount');
+  // }, []);
 
-  const handleClick = () => {
-    setReverse(!reverse);
-    setCount(count + 1);
-  };
+  // Com dependência - executa toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('C1:', counter, 'C2:', counter2);
+  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Contador: {count}</h1>
-
-        <button type="button" onClick={handleClick}>
-          Reverse {reverseClass}
-        </button>
-      </header>
+      <h1>
+        C1: {counter} C2: {counter2}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
     </div>
   );
 }
