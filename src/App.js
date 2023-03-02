bf5608405f66d2e69872443cc7e11eae4b8415b0
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-03-02 20:02:27 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,76 +1,47 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useState, useRef } from 'react';
 import './App.css';
+import React, { useReducer } from 'react';
 
-const Post = ({ post, handleClick }) => {
-  console.log('Filho renderizou');
-  return (
-    <div key={post.id} className="post">
-      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
-        {post.title}
-      </h1>
-      <p>{post.body}</p>
-    </div>
-  );
+const globalState = {
+  title: 'O título do contexto',
+  body: 'O body do contexto',
+  counter: 0,
 };
 
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda':
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+
+  return { ...state };
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-  const contador = useRef(0);
-
-  console.log('Pai renderizou!');
-
-  // Component did mount
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  useEffect(() => {
-    contador.current++;
-  });
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
 
   return (
-    <div className="App">
-      <h6>Renderizou: {contador.current}x</h6>
-      <p>
-        <input
-          ref={input}
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => {
-            return <Post key={post.id} post={post} handleClick={handleClick} />;
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('PT-BR'),
           })
-        );
-      }, [posts])}
-      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Inverter</button>
     </div>
   );
 }
